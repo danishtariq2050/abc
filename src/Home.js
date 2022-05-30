c2c957d030d48ee111f2a179584db916eff96de2
Muhammad Danish <muhammadd@alec.ae>
2022-05-31 02:20:05 +0500

    link external css with home component

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,14 @@
 import Logo from './logo.svg';
 import Logo2 from './logo512.png';
+// import './Home.css';
+import './App.css';
 
 function Home() {
     // return 'abc';
     return (
         <div>
             <h1>How are you</h1>
-            <h3>How are you</h3>
+            <h3 className='abc App'>How are you</h3>
             <img src={Logo} />
             <img src={Logo2} />
             <img src={Logo} />
