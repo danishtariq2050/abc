cc8b659eaf220795da2cf8ef7de2f6978d43d7e1
Muhammad Danish <muhammadd@alec.ae>
2022-06-02 01:32:30 +0500

    add html code for login page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,19 @@
 function Login() {
-    return 'login';
+    return (
+        <form className="container mt-5">
+            <h1 className="text-center">Login</h1>
+            <hr width="50" color="red" />
+            <div className="form-group mt-5">
+                <input type="email" className="form-control" placeholder="Email Address" />
+            </div>
+
+            <div className="form-group">
+                <input type="password" className="form-control" placeholder="Password" />
+            </div>
+
+            <button className="btn btn-primary btn-block mt-4">Sign In</button>
+        </form>
+    );
 }
 
 export default Login;
