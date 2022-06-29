1b20fa628c9a1ba12b995b6c2f1549bb80b814e9
Muhammad Danish <muhammadd@alec.ae>
2022-06-30 01:20:32 +0500

    add code for get all products

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Dashboard = () => {
 
     const navigateTo = useNavigate();
+    const [products, setProducts] = useState([]);
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -13,27 +16,71 @@ const Dashboard = () => {
             const user = jwt_decode(token);;
 
             if (user) {
-
+                getProducts();
             }
 
             else {
                 localStorage.removeItem('token');
-                navigateTo('/');
+                navigateTo('/login');
             }
         }
 
         else {
-            navigateTo('/');
+            navigateTo('/login');
         }
 
     }, [])
 
+    const getProducts = async () => {
+        const res = await fetch('http://localhost:5000/api/getProducts', {
+            headers: {
+                'x-access-token': localStorage.getItem('token')
+            }
+        });
+
+        const data = await res.json();
+
+        if (data.status === 'ok') {
+            setProducts(data.product);
+        }
+    }
+
     return (
-        <div className="my-3">
+        <div className="my-3 container-fluid">
             <h3 className="text-center">Welcome to Dashboard</h3>
             <hr width="50" color="red" />
 
-            <div className="mt-5"></div>
+            <div className="mt-5">
+                <Link to={"/addProduct"}>Add New Product</Link>
+
+                <table className="table table-hover table-dark">
+                    <thead>
+                        <tr>
+                            <th>S/N</th>
+                            <th>Product Name</th>
+                            <th>Model</th>
+                            <th>Category</th>
+                            <th>Price (PKR.)</th>
+                            <th>Description</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {
+                            products.map((v, i) => (
+                                <tr key={i}>
+                                    <td>{i + 1}</td>
+                                    <td>{v.name}</td>
+                                    <td>{v.model}</td>
+                                    <td>{v.category}</td>
+                                    <td>{v.price}</td>
+                                    <td>{v.description}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 }
