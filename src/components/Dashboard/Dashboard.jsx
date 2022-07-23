905776036c99683215b162773d160d62a9a3835a
Muhammad Danish <muhammadd@alec.ae>
2022-07-23 16:01:20 +0500

    change view of DashBoard

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -3,11 +3,15 @@ import { useNavigate } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import { Link } from "react-router-dom";
 import { useState } from "react";
+import NumberFormat from 'react-number-format';
+import NoPhotoImage from '../../images/nophoto.jpg';
+import './style.css';
 
 const Dashboard = () => {
 
     const navigateTo = useNavigate();
     const [products, setProducts] = useState([]);
+    const [productView, setProductView] = useState('grid');
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -45,43 +49,112 @@ const Dashboard = () => {
         }
     }
 
+    const changeProductView = (view) => {
+        setProductView(view);
+    }
+
     return (
         <div className="my-3 container-fluid">
             <h3 className="text-center">Welcome to Dashboard</h3>
             <hr width="50" color="red" />
 
             <div className="mt-5">
-                <Link to={"/addProduct"}>Add New Product</Link>
+                <div className="d-flex flex-row justify-content-end mb-3">
+                    <div class="btn-group mr-3">
+                        <button
+                            type="button"
+                            class="btn btn-secondary"
+                            disabled={productView === 'list'}
+                            onClick={() => changeProductView('list')}>
+                            <i class="fas fa-list"></i>
+                        </button>
+                        <button
+                            type="button"
+                            class="btn btn-secondary"
+                            disabled={productView === 'grid'}
+                            onClick={() => changeProductView('grid')}>
+                            <i class="fas fa-grip-horizontal"></i>
+                        </button>
+                    </div>
+                    <Link to={"/addProduct"}>
+                        <button className="btn btn-outline-success">Add New Product</button>
+                    </Link>
+                </div>
 
-                <table className="table table-hover table-dark">
-                    <thead>
-                        <tr>
-                            <th>S/N</th>
-                            <th>Product Name</th>
-                            <th>Model</th>
-                            <th>Category</th>
-                            <th>Price (PKR.)</th>
-                            <th>Description</th>
-                        </tr>
-                    </thead>
+                {
+                    productView === 'grid' && (
+                        <div className="row">
+                            {
+                                products.map((v, i) => (
+                                    <div className="col-3" key={i}>
+                                        <div class="image-container">
+                                            <img
+                                                src={v.image ? v.image : NoPhotoImage}
+                                                alt={v.name + ' Product'}
+                                                className="img-fluid h-50 image" />
+                                            <div class="overlay">
+                                                <div class="icons">
+                                                    <i className="fas fa-eye text-primary"></i>
+                                                    <i className="fas fa-edit mx-4 text-warning"></i>
+                                                    <i className="fas fa-trash text-dark"></i>
+                                                </div>
+                                            </div>
+                                        </div>
 
-                    <tbody>
-                        {
-                            products.map((v, i) => (
-                                <tr key={i}>
-                                    <td>{i + 1}</td>
-                                    <td>{v.name}</td>
-                                    <td>{v.model}</td>
-                                    <td>{v.category}</td>
-                                    <td>{v.price}</td>
-                                    <td>{v.description}</td>
+                                        <hr />
+                                        <h4>{v.name} &mdash; {v.model}</h4>
+                                        <small>{v.category}</small>
+                                        <p className="text-right">
+                                            <NumberFormat
+                                                className='text-danger font-weight-bold'
+                                                decimalScale={2}
+                                                fixedDecimalScale={true}
+                                                value={v.price}
+                                                displayType={'text'}
+                                                thousandSeparator={true}
+                                                prefix={'PKR '} />
+                                        </p>
+                                        <p>{v.description}</p>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    )
+                }
+
+                {
+                    productView === 'list' && (
+                        <table className="table table-hover table-dark">
+                            <thead>
+                                <tr>
+                                    <th>S/N</th>
+                                    <th>Product Name</th>
+                                    <th>Model</th>
+                                    <th>Category</th>
+                                    <th>Price (PKR.)</th>
+                                    <th>Description</th>
                                 </tr>
-                            ))
-                        }
-                    </tbody>
-                </table>
+                            </thead>
+
+                            <tbody>
+                                {
+                                    products.map((v, i) => (
+                                        <tr key={i}>
+                                            <td>{i + 1}</td>
+                                            <td>{v.name}</td>
+                                            <td>{v.model}</td>
+                                            <td>{v.category}</td>
+                                            <td>{v.price}</td>
+                                            <td>{v.description}</td>
+                                        </tr>
+                                    ))
+                                }
+                            </tbody>
+                        </table>
+                    )
+                }
             </div>
-        </div>
+        </div >
     )
 }
 
